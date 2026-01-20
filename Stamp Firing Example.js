9b5cb97c549a5da43567c5c708cfbc3b936af0f9
MW <73121152+7JD-MW@users.noreply.github.com>
2026-01-20 14:53:55 -0500

    Enhance comments for stamp firing example
    Added comments to explain the stamp firing process and usage.

---
+++ Stamp Firing Example.js
@@ -0,0 +1,9 @@
+//Create a new stamp with a text field. Go to Tools -> Organize pages -> Page Templates. Rename the newly created stamp to #FireTest
+//Switch to prepare a form mode. Right click the text field, go to properties, calculate. In custom calculation script add the below code.
+//This will show the script firing when opening the ribbon "Prefire". As well as the actual fire when stamping. 
+if( event.source.forReal && event.source.stampName == "#FireTest"){
+    event.value = event.source.stampName;
+    console.println("Event Stamp forReal: " + event.source.stampName);
+} else{
+    console.println("Event Stamp Prefire: " + event.source.stampName);
+}
